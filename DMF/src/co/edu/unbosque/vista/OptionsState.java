aeae4f3ecd3a543b9a77279bba720e18053eeae8
Javi1f <javifemeza76@gmail.com>
2022-11-07 15:33:48 -0500

    se cambiaron los gifs para la pestaña de options y title

--- DMF/src/co/edu/unbosque/vista/OptionsState.java
+++ DMF/src/co/edu/unbosque/vista/OptionsState.java
@@ -1,7 +1,37 @@
 package co.edu.unbosque.vista;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
 
-public class OptionsState extends JPanel{
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
+public class OptionsState extends JLayeredPane {
+
+	private JLabel opOptionsTitle,opBackground;
+	
+	
+	public OptionsState() {
+	setBounds(0,0,1024,768);
+	setLayout(null);
+	setBackground(Color.DARK_GRAY);
+
+	opOptionsTitle = new JLabel();
+	opOptionsTitle.setFont(new Font("Alagard", Font.ITALIC, 40));
+	opOptionsTitle.setForeground(Color.WHITE);
+	opOptionsTitle.setText("Options");
+	opOptionsTitle.setSize(1024, 40);
+	opOptionsTitle.setLocation(30, 30);
+	
+	opBackground = new JLabel(new ImageIcon("src/Assets/CQx.gif"));
+	opBackground.setBounds(0, 0, 1024, 768);
+	
+	setVisible(true);
+	
+	add(opOptionsTitle);
+	add(opBackground);
+	
+	}
 
 }
--- DMF/src/co/edu/unbosque/vista/TitleState.java
+++ DMF/src/co/edu/unbosque/vista/TitleState.java
@@ -8,35 +8,34 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 
-public class TitleState extends JLayeredPane{
-	//Components
+public class TitleState extends JLayeredPane {
+	// Components
 	private JButton tsNewGame, tsOptions, tsTutorial, tsQuit, tsCreadits;
 	private JLabel tsLogo, tsBackground, tsLogoShadow;
-	Sound sound = new Sound();
-	
+
 	public TitleState() {
 		setSize(1024, 768);
-		setLocation(0,0);
+		setLocation(0, -5);
 		setLayout(null);
 		setBackground(Color.DARK_GRAY);
-		
-		tsBackground = new JLabel(new ImageIcon("src/Assets/tsBackgroundImage.gif"));
-		tsBackground.setBounds(0,0, 1024, 768);
+
+		tsBackground = new JLabel(new ImageIcon("src/Assets/bosque.gif"));
+		tsBackground.setBounds(0, 0, 1024, 768);
 		
 		tsLogo = new JLabel();
 		tsLogo.setFont(new Font("Alagard", Font.ITALIC, 40));
 		tsLogo.setForeground(Color.WHITE);
 		tsLogo.setText("Dungeons,\n Mazes and Forests");
 		tsLogo.setSize(1024, 40);
-		tsLogo.setLocation(30,30);
-		
+		tsLogo.setLocation(30, 30);
+
 		tsLogoShadow = new JLabel();
 		tsLogoShadow.setFont(new Font("Alagard", Font.ITALIC, 40));
 		tsLogoShadow.setForeground(Color.DARK_GRAY);
 		tsLogoShadow.setText("Dungeons,\n Mazes and Forests");
 		tsLogoShadow.setSize(1024, 40);
 		tsLogoShadow.setLocation(33, 33);
-		
+
 		tsNewGame = new JButton();
 		tsNewGame.setSize(250, 20);
 		tsNewGame.setText("New Game");
@@ -45,7 +44,7 @@ public class TitleState extends JLayeredPane{
 		tsNewGame.setForeground(Color.WHITE);
 		tsNewGame.setLocation(30, 150);
 		tsNewGame.setBorderPainted(false);
-		
+
 		tsOptions = new JButton();
 		tsOptions.setSize(250, 20);
 		tsOptions.setText("Options");
@@ -54,7 +53,7 @@ public class TitleState extends JLayeredPane{
 		tsOptions.setForeground(Color.WHITE);
 		tsOptions.setLocation(30, 190);
 		tsOptions.setBorderPainted(false);
-		
+
 		tsTutorial = new JButton();
 		tsTutorial.setSize(250, 20);
 		tsTutorial.setText("How to Play");
@@ -63,7 +62,7 @@ public class TitleState extends JLayeredPane{
 		tsTutorial.setForeground(Color.WHITE);
 		tsTutorial.setLocation(30, 230);
 		tsTutorial.setBorderPainted(false);
-		
+
 		tsCreadits = new JButton();
 		tsCreadits.setSize(250, 20);
 		tsCreadits.setText("Credits");
@@ -72,7 +71,7 @@ public class TitleState extends JLayeredPane{
 		tsCreadits.setForeground(Color.WHITE);
 		tsCreadits.setLocation(30, 270);
 		tsCreadits.setBorderPainted(false);
-		
+
 		tsQuit = new JButton();
 		tsQuit.setSize(250, 20);
 		tsQuit.setText("Quit");
@@ -81,33 +80,85 @@ public class TitleState extends JLayeredPane{
 		tsQuit.setForeground(Color.WHITE);
 		tsQuit.setLocation(30, 310);
 		tsQuit.setBorderPainted(false);
-		
-		playMusic(0);
-		
-		//Background
+			
+		// Background
 		add(tsBackground, JLayeredPane.DEFAULT_LAYER);
-		
-		//Buttons
+
+		// Buttons
 		add(tsNewGame, JLayeredPane.MODAL_LAYER);
 		add(tsOptions, JLayeredPane.MODAL_LAYER);
 		add(tsTutorial, JLayeredPane.MODAL_LAYER);
 		add(tsQuit, JLayeredPane.MODAL_LAYER);
 		add(tsCreadits, JLayeredPane.MODAL_LAYER);
-		
-		//Title
+
+		// Title
 		add(tsLogoShadow, JLayeredPane.PALETTE_LAYER);
 		add(tsLogo, JLayeredPane.MODAL_LAYER);
-	}
-	
-	public void playMusic(int i) {
 		
-		sound.setFile(i);
-		sound.play();
-		sound.loop();
 	}
-	public void stopMusic() {
-		
-		sound.stop();
+
+	public JButton getTsNewGame() {
+		return tsNewGame;
 	}
-	
+
+	public void setTsNewGame(JButton tsNewGame) {
+		this.tsNewGame = tsNewGame;
+	}
+
+	public JButton getTsOptions() {
+		return tsOptions;
+	}
+
+	public void setTsOptions(JButton tsOptions) {
+		this.tsOptions = tsOptions;
+	}
+
+	public JButton getTsTutorial() {
+		return tsTutorial;
+	}
+
+	public void setTsTutorial(JButton tsTutorial) {
+		this.tsTutorial = tsTutorial;
+	}
+
+	public JButton getTsQuit() {
+		return tsQuit;
+	}
+
+	public void setTsQuit(JButton tsQuit) {
+		this.tsQuit = tsQuit;
+	}
+
+	public JButton getTsCreadits() {
+		return tsCreadits;
+	}
+
+	public void setTsCreadits(JButton tsCreadits) {
+		this.tsCreadits = tsCreadits;
+	}
+
+	public JLabel getTsLogo() {
+		return tsLogo;
+	}
+
+	public void setTsLogo(JLabel tsLogo) {
+		this.tsLogo = tsLogo;
+	}
+
+	public JLabel getTsBackground() {
+		return tsBackground;
+	}
+
+	public void setTsBackground(JLabel tsBackground) {
+		this.tsBackground = tsBackground;
+	}
+
+	public JLabel getTsLogoShadow() {
+		return tsLogoShadow;
+	}
+
+	public void setTsLogoShadow(JLabel tsLogoShadow) {
+		this.tsLogoShadow = tsLogoShadow;
+	}
+
 }
