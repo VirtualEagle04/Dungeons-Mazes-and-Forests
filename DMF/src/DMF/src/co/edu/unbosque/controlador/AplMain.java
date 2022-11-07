339245543a8d8ad703c5541a89b7429e0b2efd7c
VirtualEagle <federicovargasrozo@gmail.com>
2022-11-07 16:08:25 -0500

    Corregí la duplicación rara

--- DMF/src/DMF/src/co/edu/unbosque/controlador/AplMain.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unbosque.controlador;
-
-public class AplMain {
-	public static void main(String[] args) {
-		Controlador c = new Controlador();
-	}
-}
--- DMF/src/DMF/src/co/edu/unbosque/vista/GameState.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unbosque.vista;
-
-import javax.swing.JPanel;
-
-public class GameState extends JPanel{
-
-}
